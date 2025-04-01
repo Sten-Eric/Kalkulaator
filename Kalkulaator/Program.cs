e6c61abc5067f8f31624257fc22625b8cf4566f1
opilane <opilane@HP-CZC321706B>
2025-04-01 13:54:54 +0300

    Add project files.

---
+++ Kalkulaator/Program.cs
@@ -0,0 +1,12 @@
+﻿esimene = int(input("Esimene arv: "))
+teine = int(input("Teine arv: "))
+kolmas = input("Kas (Korruta,Jaga,Liida,Lahuta): ")
+
+if kolmas == "Korruta":
+    print(esimene * teine)
+if kolmas == "Jaga":
+    print(esimene / teine)
+if kolmas == "Liida":
+    print(esimene + teine)
+if kolmas == "Lahuta":
+    print(esimene - teine)
